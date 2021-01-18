5dd07f0338385252e09d899ed03dcc36d46f32a0
Yuto Omachi <youomachi@gmail.com>
2021-01-18 23:37:13 +0800

    created separate file for socket server side

--- server/app.js
+++ server/app.js
@@ -2,13 +2,11 @@ const express= require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const socket = require('./socket')(io);
 const path = require('path');
-
-const Message = require('./Message.js');
 
 require('dotenv').config()
 
-const messages = [];
 const hostname = 'localhost';;
 const port = process.env.PORT || 3000;
 
@@ -17,10 +15,9 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-// app.get('/', (req,res) => {
-//     let reqPath = path.join(__dirname, '../client/index.html')
-//     res.sendFile(reqPath);
-// })
+app.get('/', (req,res) => {
+    res.redirect("/login");
+})
 
 app.get('/login', (req,res)=>{
     let reqPath = path.join(__dirname, '../client/login.html')
@@ -32,27 +29,7 @@ app.post('/', (req, res) => {
     res.render(reqPath, req.body);
 })
 
-//Whenever someone connects this gets executed
-io.sockets.on('connection', function(socket) {
-    socket.on('join', ()=>{
-        socket.emit('loadMessage', messages);
-    });
 
-    //When char message event is sent
-    socket.on('newMessage', (id, msg)=>{
-        let message = new Message(id, msg);
-        messages.push(message);
-        io.emit('newMessage', id, msg);
-    })
-
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-    });
-
-    socket.on('error', function (err) {
-        console.log(err);
-    });
- });
 
  
 
